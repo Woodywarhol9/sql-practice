8ba9d5c79afe4eec592826b4b3305d0fdc6dd561
Woodywarhol9 <woodywarhol9@gmail.com>
2022-04-19 15:14:05 +0900

    Upload 입양_시각_구하기_2.sql

---
+++ programmers/group_by/입양_시각_구하기_2.sql
@@ -0,0 +1,7 @@
+set @hour_list = -1;
+
+select (@hour_list := @hour_list + 1) as "hour", 
+(select count(*) from animal_outs where hour(datetime) = @hour_list ) as "hour"
+    from animal_outs
+    where @hour_list <=22
+    order by hour;
